30b1b39401f62909b9d4c0f6461462f75e49cf48
MuneebUllah <98342920+MuneebUllah@users.noreply.github.com>
2023-11-18 22:32:16 +0500

    updates

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Resturant from "./component/Basics/Resturant";
+// import UseState from "./component/Hooks/useState";
+// import UseEffect from "./component/Hooks/useEffect";
+// import UseReducer from "./component/Hooks/useReducer";
+// import  Restaurant from "./component/Basics/Resturant";
+
+const App = () => {
+  return <Resturant />;
+};
+
+export default App;
